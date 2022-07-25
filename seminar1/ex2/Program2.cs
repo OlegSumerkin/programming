49ed2fdd80e3f1095d34dc378f419ca0909449db
OlegSumerkin <oleg.sumerkin1@gmail.com>
2022-07-25 17:22:19 +0300

    something

--- seminar1/ex2/Program2.cs
+++ seminar1/ex2/Program2.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите Ваше имя: ");
+console.Write("Введите Ваше имя: ");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.WriteLine(username);
--- seminar1/ex3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int numberA = 9;
-int numberB = 14;
-Console.Write("Summary: ");
-Console.WriteLine(numberA + nimberB);
